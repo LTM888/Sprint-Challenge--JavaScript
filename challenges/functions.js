0fd9d3e5cd92598aa559c2f08e8c7e7602df5684
LTM888 <leigh.tm@gmail.com>
2019-03-08 12:25:17 -0500

    object-arrays is done and now functions now for prorotypes

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,24 +1,42 @@
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+ * Create a higher-order function named consume that can take 3 parameters.
+ * The first two parameters can accept any argument
+ * The last parameter accepts a callback 
+ * In the body of the function return the callback with the two parameters that you created
+ */
 
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers 
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+function consume(test1, test2, callbackFunction) {
+  return callbackFunction(test1, test2);
+
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(name, lastName) {
+  return `Hello ${name} ${lastName}, nice to meet you `;
+}
 
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+
+// Step 3: Check your work by un-commenting the following calls to consume()
+
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +44,8 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// I think that the reason 'nestedFunction' can access 'internal' is due to local scope.
+// both are in the closures of  'external'.
 
 
 const external = "I'm outside the function";
